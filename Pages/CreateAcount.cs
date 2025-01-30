5de2bfbf20ea7a172f36fabfbe1c24f1893fea6f
AriJohns03 <ajohns@student.neumont.edu>
2025-01-30 13:48:38 -0700

    Create Account Class

--- Pages/CreateAcount.cs
+++ Pages/CreateAcount.cs
@@ -2,6 +2,17 @@
 {
     public class CreateAcount
     {
+        private static int nextID = 0;
+        public int Id { get; set; } = nextID++;
 
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateOnly DateOfBirth { get; set; }
+
+        public string Email { get; set; }
+
+        public string CompanyName { get; set; }
     }
 }
